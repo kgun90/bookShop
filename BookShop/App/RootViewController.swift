d42fe1689cae6304cd4865cb808865e10d8a5b6f
Geon Kang <kgun90@gmail.com>
2024-06-14 10:05:03 +0900

    feat: Initial Commit

---
+++ BookShop/App/RootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RootViewController.swift
+//  BookShop
+//
+//  Created by gkang on 6/10/24.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        interface()
+    }
+
+    private func setup() {
+        
+    }
+    
+    private func interface() {
+        
+    }
+}
+
